685f186f7f38cc3ce2e9d163c5c4b606929127c7
root <root@PatrickBateman.(none)>
2013-05-03 18:22:01 -0400

    Added additional private API files.  Use ajax calls. GetAllHeroGuides returns all guides in JSON GetHeroGuide Requires a heroName and returns a guide in JSON GetSuggestionGuide requires a suggestion ID and returns a gudie in JSON

---
+++ php/GetAllHeroGuides.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'constants.php';
+$dbh = new mysqli(HOST,USER,PW, DB);
+$output;
+if ($dbh->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $dbh->connect_errno . ") " . $dbh->connect_error;
+	exit;
+}
+
+if (!$stmt = $dbh->prepare("SELECT G.guideID, G.heroName, G.buildName, G.skillBuild, G.starting, G.core, G.extension, G.notes FROM guide G, hero H WHERE G.guideID = H.guideID")) {
+	echo "Prepare failed: (" . $dbh->errno . ") " . $dbh->error;
+	exit;
+}
+elseif (!$stmt->execute()) {
+	echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+	exit;
+}
+elseif(!$stmt->bind_result($a, $b, $c, $d, $e, $f, $g, $h)){
+	echo "Bind Result failed: (" . $stmt->errno . ") " . $stmt->error;
+	exit;
+}
+else{
+	while ($row = $stmt->fetch()) {
+		$output = json_encode(array(
+					   "guideID" => $a,
+					   "heroID" => $b,
+					   "buildName" => $c, 
+					   "skillBuild" => $d, 
+					   "starting" => $e, 
+					   "core" => $f, 
+					   "extension" => $g, 
+					   "notes" => $h
+					   ));
+	  }
+}
+return $output;
+
+function getInput($input) {
+	$temp = strip_tags(substr($_POST[$input],0, 100));
+	$temp = mysql_real_escape_string($temp);
+	return $temp;
+}
+?>
